9786f51112d87caa2843886e0a0b6d75ffc74aa0
amcsparron2793-Water <78561192+amcsparron2793-Water@users.noreply.github.com>
2024-05-03 07:01:28 -0400

    Update GalacticSalvage.py
    removed Player class from main GalacticSalvage.py module

--- GalacticSalvage/GalacticSalvage.py
+++ GalacticSalvage/GalacticSalvage.py
@@ -26,30 +26,7 @@ class Settings:
     SCREEN_HEIGHT = 600
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Galactic Salvage")
-
-
-class Player:
-    def __init__(self, color: Tuple[int, int, int] = (0, 255, 0), projectile_cooldown: int = 15):
-        self.width = 50
-        self.height = 50
-        self.color = color
-        self.x = Settings.SCREEN_WIDTH // 2 - self.width // 2
-        self.y = Settings.SCREEN_HEIGHT - self.height - 20
-        self.speed = 5
-        self.projectile_cooldown = projectile_cooldown  # Cooldown period in frames
-        self.cooldown_counter = 0
-        self.blaster = pygame.mixer.Sound('../Misc_Project_Files/sounds/blaster.mp3')
-        self.boom = pygame.mixer.Sound('../Misc_Project_Files/sounds/BoomPlayer.mp3')
-        # TODO: add score var etc.
-
-    def move_left(self):
-        if self.x > 0:
-            self.x -= self.speed
-
-    def move_right(self):
-        if self.x < Settings.SCREEN_WIDTH - self.width:
-            self.x += self.speed
-
+    
 
 class Projectile:
     def __init__(self, x, y, color: Tuple[int, int, int] = (255, 0, 0)):
