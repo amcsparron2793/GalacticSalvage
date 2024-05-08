dd42ce89740b317bafa9c38d65d0cb1aa37421d5
amcsparron2793 <69968398+amcsparron2793@users.noreply.github.com>
2024-05-07 20:24:19 -0400

    added asteroid png

--- GalacticSalvage/Asteroid.py
+++ GalacticSalvage/Asteroid.py
@@ -1,4 +1,6 @@
-from pygame import Surface, SRCALPHA, draw, transform, Rect
+from os.path import isfile
+
+from pygame import Surface, SRCALPHA, draw, transform, Rect, image
 import random
 from pygame.sprite import Sprite
 
@@ -10,16 +12,20 @@ class Asteroid(Sprite):
         self.width = random.randint(20, 50)
         self.height = random.randint(20, 50)
         self.color = (150, 150, 150)
-        self.image = Surface((self.width, self.height), SRCALPHA)
-        draw.rect(self.image, self.color, (0, 0, self.width, self.height))
+        if not isfile('../Misc_Project_Files/images/SingleAsteroid.png'):
+            self.image = Surface((self.width, self.height), SRCALPHA)
+            draw.rect(self.image, self.color, (0, 0, self.width, self.height))
+        else:
+            self.image = image.load('../Misc_Project_Files/images/SingleAsteroid.png')
+            self.image = transform.scale_by(self.image, 0.05)
         self.rect = self.image.get_rect()
         self.rect.x = random.randint(0, self.settings.screen_width - self.width)
         self.rect.y = -self.settings.screen_height - self.height  # Start above the screen
         self.speed = random.randint(1, 3)
         self.angle = 0
-        self.rotation_speed = random.uniform(-0.5, 0.5)  # Random rotation speed
+        self.rotation_speed = random.uniform(-1, 1)  # Random rotation speed
 
     def update(self):
         self.rect.y += self.speed
         self.angle += self.rotation_speed
-        #self.image = transform.rotate(self.image, self.angle)
+        # self.image = transform.rotate(self.image, self.angle)
