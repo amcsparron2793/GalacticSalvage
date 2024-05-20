41c1aff1b014b70a39e95da93f5213917eb0a9f8
amcsparron2793 <69968398+amcsparron2793@users.noreply.github.com>
2024-05-20 13:35:20 -0400

    added GPT generated InputBox class and Leaderboard method to implement it. needs to be tweaked.

--- GalacticSalvage/Leaderboard.py
+++ GalacticSalvage/Leaderboard.py
@@ -1,5 +1,44 @@
 from SQLLite3HelperClass import SQLlite3Helper
+from pygame import Rect, font, draw
+from pygame.constants import *
 from typing import List
+
+
+class InputBox:
+    # TODO: need get player name - this is GPT generated, needs to be integrated.
+    def __init__(self, x, y, w, h, font_size=74, text=''):
+        self.rect = Rect(x, y, w, h)
+        self.color = (0, 0, 0)
+        self.text = text
+        self.font = font.Font(None, font_size)
+        self.txt_surface = self.font.render(text, True, self.color)
+        self.active = False
+
+    def handle_event(self, event):
+        if event.type == MOUSEBUTTONDOWN:
+            if self.rect.collidepoint(event.pos):
+                self.active = not self.active
+            else:
+                self.active = False
+            self.color = (200, 200, 200) if self.active else (0, 0, 0)
+
+        if event.type == KEYDOWN:
+            if self.active:
+                if event.key == K_RETURN:
+                    return self.text  # Return the text when Enter is pressed
+                elif event.key == K_BACKSPACE:
+                    self.text = self.text[:-1]
+                else:
+                    self.text += event.unicode
+                self.txt_surface = self.font.render(self.text, True, self.color)
+
+    def update(self):
+        width = max(200, self.txt_surface.get_width()+10)
+        self.rect.w = width
+
+    def draw(self, screen):
+        screen.blit(self.txt_surface, (self.rect.x+5, self.rect.y+5))
+        draw.rect(screen, self.color, self.rect, 2)
 
 
 class Leaderboard:
@@ -58,6 +97,34 @@ class Leaderboard:
         print(q_str)
         self.sql.Query(q_str)
         self.cxn.commit()
+
+    def get_player_name(self):
+        # TODO: this needs to be tweaked to work with the game - GPT generated.
+        input_box = InputBox(100, 100, 140, 50)
+        clock = pygame.time.Clock()
+        player_name = None
+        done = False
+
+        while not done:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    return None
+                result = input_box.handle_event(event)
+                if result:
+                    player_name = result
+                    done = True
+
+            input_box.update()
+            self.settings.screen.fill((255, 255, 255))
+            input_box.draw(self.settings.screen)
+
+            prompt_surface = pygame.font.Font(None, 50).render('Enter your name:', True, (0, 0, 0))
+            self.settings.screen.blit(prompt_surface, (100, 50))
+
+            pygame.display.flip()
+            clock.tick(30)
+
 
 
 
