1d20099eabc4c1db2d883d18f96b94c996eddb46
amcsparron2793 <69968398+amcsparron2793@users.noreply.github.com>
2024-05-19 20:01:00 -0400

    added CreateTables.sql

---
+++ Misc_Project_Files/CreateTables.sql
@@ -0,0 +1,13 @@
+create table Players(id integer primary key autoincrement,
+                     player_name nvarchar(50) not null unique);
+create table Leaderboard(id integer primary key autoincrement,
+                         GameDate date not null default current_date,
+                         player_id integer not null references Players(id),
+                         score integer not null,
+                         level integer not null
+                         );
+
+create view FullLeaderboard as
+    select P.player_name, GameDate, score, level
+    from Leaderboard
+        join Players P on P.id = Leaderboard.player_id;
